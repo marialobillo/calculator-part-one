2fe54e07407aaff27d3c47c9daffd27a3c1ee3f5
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-28 11:27:40 +0100

    test cases for factorial method

--- test/index_test.js
+++ test/index_test.js
@@ -51,4 +51,22 @@ describe('Calculate', () => {
     });
   });
 
+  describe('.factorial', () => {
+    it('return the output for 5!', () => {
+      const expected = Calculate.factorial(5);
+      result = 120;
+      assert.equal(expected, result);
+    });
+    it('return the output for 4!', () => {
+      const expected = Calculate.factorial(4);
+      result = 24;
+      assert.equal(expected, result);
+    });
+    it('return the 0!', () => {
+      const expected = Calculate.factorial(0);
+      result = 1;
+      assert.equal(expected, result);
+    });
+  });
+
 });
